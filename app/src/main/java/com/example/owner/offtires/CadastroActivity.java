337bc2cdad1039b432a6425149e1a8d7fabee6fb
Paulo Matheus <paulo_smelo@outlook.com>
2018-11-28 20:13:53 -0200

    telas de login e cadastro

--- app/src/main/java/com/example/owner/offtires/CadastroActivity.java
+++ app/src/main/java/com/example/owner/offtires/CadastroActivity.java
@@ -1,13 +1,25 @@
 package com.example.owner.offtires;
 
-import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class CadastroActivity extends AppCompatActivity {
-
+    TextView mytv;
+    Typeface MyFont;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+        mytv = (TextView)findViewById(R.id.btnEntrar);
+        MyFont = Typeface.createFromAsset(this.getAssets(), "fonts/seguili.ttf");
+        mytv.setTypeface(MyFont);
+    }
+    public void Entrar(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/owner/offtires/LoginActivity.java
+++ app/src/main/java/com/example/owner/offtires/LoginActivity.java
@@ -1,13 +1,34 @@
 package com.example.owner.offtires;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView mytv;
+    TextView mytv2;
+    Typeface MyFont;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mytv = (TextView)findViewById(R.id.txtCadastro);
+        mytv2 = (TextView)findViewById(R.id.btnEntrar);
+        MyFont = Typeface.createFromAsset(this.getAssets(), "fonts/seguili.ttf");
+        mytv.setTypeface(MyFont);
+        mytv2.setTypeface(MyFont);
+    }
+    public void Entrar(View view) {
+       Intent intent = new Intent(this, HomeActivity.class);
+       startActivity(intent);
+
+    }
+    public void Cadastro(View view) {
+        Intent intent = new Intent(this, CadastroActivity.class);
+        startActivity(intent);
     }
 }
